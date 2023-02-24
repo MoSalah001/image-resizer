a1e02e607df74d85f17ce1efd53a859e900b77ad
MoSalah001 <mhmdsalah707@gmail.com>
2023-02-24 16:35:49 +0200

    edit host and port for deployment

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,15 @@ import routes from './routes/main';
 import path from 'path';
 
 const app = express();
-const port = 3000;
+const host = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.dirname(__dirname) + '/client'));
 
 app.use('/api', routes);
 
-app.listen(port, () => {
-  console.log(`server is running on http://localhost:${port}`);
+app.listen(port, host, () => {
+  console.log(`server is running on ${host}:${port}`);
 });
 
 export default app;
