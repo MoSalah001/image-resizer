f4e47586bf0ea8872c460b2d3756923e634fcfac
MoSalah001 <mhmdsalah707@gmail.com>
2023-02-26 18:52:36 +0200

    optimize build

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,14 @@ import routes from './routes/main';
 import path from 'path';
 
 const app = express();
-const host = process.env.HOST || '0.0.0.0'
 const port: any  = process.env.PORT || 3000;
 
 app.use(express.static(path.dirname(__dirname) + '/client'));
 
 app.use('/api', routes);
 
-app.listen(port, host, () => {
-  console.log(`server is running on ${host}:${port}`);
+app.listen(port, () => {
+  console.log(`server is running on Port:${port}`);
 });
 
 export default app;
